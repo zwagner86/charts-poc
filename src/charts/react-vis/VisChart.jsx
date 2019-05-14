54d1f5fad9a99a7a134f531d96e4f9d80ac80c3b
Zach Wagner <zach.wagner@spothero.com>
2019-05-13 21:32:44 -0500

    use crosshair

--- src/charts/react-vis/VisChart.jsx
+++ src/charts/react-vis/VisChart.jsx
@@ -8,7 +8,8 @@ import {
     LineMarkSeries,
     HorizontalGridLines,
     VerticalRectSeries,
-    Hint
+    DiscreteColorLegend,
+    Crosshair
 } from 'react-vis';
 import styles from '../styles';
 import chartData from '../chartData';
@@ -22,29 +23,39 @@ export default class VisChart extends Component {
 
     };
     state = {
-        tooltipValue: null
+        crosshairValues: []
     };
 
     constructor(props) {
         super(props);
 
-        this._mappedData = chartData.inventoryData.map((point, i, arr) => {
+        const {
+            inventoryData,
+            randomData
+        } = chartData;
+
+        this._mappedInventoryData = inventoryData.map((point, i, arr) => {
             return {
                 x: new Date(point.x),
                 y: point.y,
                 size: ((i > 0) && (arr[i - 1].y === point.y)) ? 0 : 5
             };
         });
-
-        this._mappedRandomData = chartData.randomData.map(point => {
+        this._mappedRandomData = randomData.map(point => {
             const date = new Date(point.x);
 
             return {
-                x0: moment(date).subtract(15, 'm').toDate(),
-                x: moment(date).add(15, 'm').toDate(),
+                x0: moment(date).subtract(15, 'm')
+                    .toDate(),
+                x: moment(date).add(15, 'm')
+                    .toDate(),
                 y: point.y
             };
         });
+        this._SERIES = [
+            {title: 'Inventory', data: this._mappedInventoryData},
+            {title: 'Random', data: this._mappedRandomData},
+        ];
 
         this.tickValues = [
             new Date('2019-05-09T00:00'),
@@ -59,32 +70,48 @@ export default class VisChart extends Component {
         ];
 
         this.xDomain = [
-            moment(this.tickValues[0]).subtract(30, 'm').toDate(),
-            moment(this.tickValues[this.tickValues.length - 1]).add(30, 'm').toDate()
+            moment(this.tickValues[0]).subtract(30, 'm')
+                .toDate(),
+            moment(this.tickValues[this.tickValues.length - 1]).add(30, 'm')
+                .toDate()
         ];
     }
 
-    _onSetTooltip = tooltipValue => {
+    _onMouseLeaveHandler = () => {
         this.setState({
-            tooltipValue
+            crosshairValues: []
         });
-    }
+    };
 
-    _onForgetTooltip = () => {
+    _onNearestXHandler = (value, {index}) => {
         this.setState({
-            tooltipValue: null
+            crosshairValues: this._SERIES.map(s => s.data[index])
         });
-    }
+    };
+
+    _formatCrosshairItems = values => {
+        return values.map((v, i) => {
+            return {
+                title: this._SERIES[i].title,
+                value: v.y
+            };
+        });
+    };
+
+    _formatCrosshairTitle = values => {
+        return {
+            title: 'Time',
+            value: moment(values[0].x).format('h:mma')
+        };
+    };
 
     _xTickFormat = value => {
-        // console.log(value);
-
         return moment(value).format('ha');
     }
 
     render() {
         const {
-            tooltipValue
+            crosshairValues
         } = this.state;
         const {
             colorGo,
@@ -95,38 +122,51 @@ export default class VisChart extends Component {
         return (
             <div className="VisChart">
                 <h1>React-vis</h1>
-                <div className="VisChart-chart-container">
-                    {/* eslint-disable react/forbid-component-props */}
-                    <FlexibleXYPlot xDomain={this.xDomain}>
-                        <XAxis
-                            tickFormat={this._xTickFormat}
-                            tickValues={this.tickValues}
-                        />
-                        <YAxis />
-                        <HorizontalGridLines />
-                        <VerticalRectSeries
-                            className="VisChart-rect"
-                            data={this._mappedRandomData}
-                            style={{
-                                stroke: colorWhite,
-                                fill: colorCaution
-                            }}
-                        />
-                        <LineMarkSeries
-                            className="VisChart-lines"
-                            data={this._mappedData}
-                            curve="curveStepAfter"
-                            color={colorGo}
-                            fill={colorWhite}
-                            sizeType="literal"
-                            onValueMouseOver={this._onSetTooltip}
-                            onValueMouseOut={this._onForgetTooltip}
-                        />
-                        {tooltipValue &&
-                            <Hint value={tooltipValue} />
-                        }
-                    </FlexibleXYPlot>
-                    {/* eslint-enable react/forbid-component-props */}
+                <div className="VisChart-content">
+                    <DiscreteColorLegend
+                        orientation="horizontal"
+                        items={[
+                            {title: 'Inventory', color: colorGo},
+                            {title: 'Random', color: colorCaution},
+                        ]}
+                    />
+                    <div className="VisChart-chart-container">
+                        {/* eslint-disable react/forbid-component-props */}
+                        <FlexibleXYPlot
+                            xDomain={this.xDomain}
+                            onMouseLeave={this._onMouseLeaveHandler}
+                        >
+                            <XAxis
+                                tickFormat={this._xTickFormat}
+                                tickValues={this.tickValues}
+                            />
+                            <YAxis />
+                            <HorizontalGridLines />
+                            <VerticalRectSeries
+                                className="VisChart-rect"
+                                data={this._mappedRandomData}
+                                onNearestX={this._onNearestXHandler}
+                                style={{
+                                    stroke: colorWhite,
+                                    fill: colorCaution
+                                }}
+                            />
+                            <LineMarkSeries
+                                className="VisChart-lines"
+                                data={this._mappedInventoryData}
+                                curve="curveStepAfter"
+                                color={colorGo}
+                                fill={colorWhite}
+                                sizeType="literal"
+                            />
+                            <Crosshair
+                                itemsFormat={this._formatCrosshairItems}
+                                titleFormat={this._formatCrosshairTitle}
+                                values={crosshairValues}
+                            />
+                        </FlexibleXYPlot>
+                        {/* eslint-enable react/forbid-component-props */}
+                    </div>
                 </div>
             </div>
         );
