092fbca3adbc2c57522af65ec45728176801ae9e
Zach Wagner <zach.wagner@spothero.com>
2019-05-15 16:14:31 -0500

    add second chartist chart

--- src/charts/chartist/ChartistChart.jsx
+++ src/charts/chartist/ChartistChart.jsx
@@ -24,14 +24,25 @@ export default class ChartistChart extends Component {
 
         const {
             chartData: {
-                inventoryData
+                inventoryData,
+                randomData
             }
         } = props;
 
-        this._highestYValue = 0;
-        this._mappedData = inventoryData.map(point => {
-            if (point.y > this._highestYValue) {
-                this._highestYValue = point.y;
+        this._maxY = 0;
+        this._mappedInventoryData = inventoryData.map(point => {
+            if (point.y > this._maxY) {
+                this._maxY = point.y;
+            }
+
+            return {
+                x: new Date(point.x),
+                y: point.y
+            };
+        });
+        this._mappedRandomData = randomData.map(point => {
+            if (point.y > this._maxY) {
+                this._maxY = point.y;
             }
 
             return {
@@ -50,80 +61,128 @@ export default class ChartistChart extends Component {
             <div className="ChartistChart">
                 <h1>Chartist</h1>
                 <div className="ChartistChart-chart-container">
-                    <SHChartist
-                        type="Line"
-                        legend={{
-                            legendNames: ['Inventory']
-                        }}
-                        tooltip={{
-                            pointClass: 'ct-circle',
-                            tooltipFnc: (label, value) => {
-                                return `${value} Spaces`;
-                            }
-                        }}
-                        data={{
-                            series: [{
-                                name: 'inventory',
-                                data: this._mappedData
-                            }]
-                        }}
-                        options={{
-                            series: {
-                                inventory: {
-                                    lineSmooth: Chartist.Interpolation.step()
+                    <div className="ChartistChart-chart-wrapper">
+                        <SHChartist
+                            className="ChartistChart-chart-bar"
+                            type="Bar"
+                            legend={{
+                                legendNames: ['Random']
+                            }}
+                            tooltip={{
+                                tooltipFnc: (label, value) => {
+                                    return `${value.split(',')[1]} Spaces`;
                                 }
-                            },
-                            high: (this._highestYValue + Math.ceil(this._highestYValue * 0.25)),
-                            low: 0,
-                            axisX: {
-                                showGrid: false,
-                                type: Chartist.FixedScaleAxis,
-                                divisor: 7,
-                                labelInterpolationFnc: value => {
-                                    return moment(value).format('ha');
+                            }}
+                            data={{
+                                series: [{
+                                    name: 'random',
+                                    data: this._mappedRandomData
+                                }]
+                            }}
+                            options={{
+                                high: (this._maxY + Math.ceil(this._maxY * 0.15)),
+                                low: 0,
+                                axisX: {
+                                    showGrid: false,
+                                    type: Chartist.FixedScaleAxis,
+                                    divisor: 8,
+                                    labelInterpolationFnc: value => {
+                                        return moment(value).format('ha');
+                                    }
+                                },
+                                axisY: {
+                                    onlyInteger: true
+                                },
+                                fullWidth: true,
+                                chartPadding: {
+                                    top: 0,
+                                    right: 20,
+                                    bottom: 60,
+                                    left: 0
+                                },
+                                height: '400px',
+                            }}
+                        />
+                        <SHChartist
+                            className="ChartistChart-chart-line"
+                            type="Line"
+                            legend={{
+                                legendNames: ['Inventory']
+                            }}
+                            tooltip={{
+                                pointClass: 'ct-circle',
+                                tooltipFnc: (label, value) => {
+                                    return `${value} Spaces`;
                                 }
-                            },
-                            axisY: {
-                                onlyInteger: true
-                            },
-                            lineSmooth: false,
-                            fullWidth: true,
-                            chartPadding: {
-                                top: 0,
-                                right: 20,
-                                bottom: 0,
-                                left: 0
-                            }
-                        }}
-                        events={[
-                            {
-                                event: 'draw',
-                                handler: pointData => {
-                                    if (pointData.type === 'point') {
-                                        const yValue = pointData.value.y;
+                            }}
+                            data={{
+                                series: [{
+                                    name: 'inventory',
+                                    data: this._mappedInventoryData
+                                }]
+                            }}
+                            options={{
+                                series: {
+                                    inventory: {
+                                        lineSmooth: Chartist.Interpolation.step()
+                                    }
+                                },
+                                high: (this._maxY + Math.ceil(this._maxY * 0.15)),
+                                low: 0,
+                                axisX: {
+                                    showGrid: false,
+                                    showLabel: false,
+                                    type: Chartist.FixedScaleAxis,
+                                    divisor: 8,
+                                    labelInterpolationFnc: value => {
+                                        return moment(value).format('ha');
+                                    }
+                                },
+                                axisY: {
+                                    onlyInteger: true,
+                                    showGrid: false,
+                                    showLabel: false,
+                                },
+                                lineSmooth: false,
+                                fullWidth: true,
+                                chartPadding: {
+                                    top: 0,
+                                    right: 20,
+                                    bottom: 60,
+                                    left: 0
+                                },
+                                height: '400px',
+                            }}
+                            events={[
+                                {
+                                    event: 'draw',
+                                    handler: pointData => {
+                                        if (pointData.type === 'point') {
+                                            const yValue = pointData.value.y;
 
-                                        // chartist logs index 0 twice, so use a toggle to only draw the first 0 index point
-                                        // otherwise continue to draw the other points with the correct radius
-                                        pointRadius = (pointData.index === 0 && firstIndexToggle)
-                                            ? 4
-                                            : (previousPointValue === yValue) ? 0 : 4;
-                                        previousPointValue = yValue;
-                                        firstIndexToggle = (pointData.index === 0) ? !firstIndexToggle : true;
+                                            // chartist logs index 0 twice, so use a toggle to only draw the first 0 index point
+                                            // otherwise continue to draw the other points with the correct radius
+                                            pointRadius = (pointData.index === 0 && firstIndexToggle)
+                                                ? 4
+                                                : (previousPointValue === yValue) ? 0 : 4;
+                                            previousPointValue = yValue;
+                                            firstIndexToggle = (pointData.index === 0) ? !firstIndexToggle : true;
 
-                                        const circle = new Chartist.Svg('circle', {
-                                            cx: [pointData.x],
-                                            cy: [pointData.y],
-                                            r: [pointRadius],
-                                            'ct:value': pointData.value.y,
-                                            'ct:meta': pointData.meta
-                                        }, 'ct-circle');
+                                            const circle = new Chartist.Svg('circle', {
+                                                cx: [pointData.x],
+                                                cy: [pointData.y],
+                                                r: [pointRadius],
+                                                'ct:value': pointData.value.y,
+                                                'ct:meta': pointData.meta
+                                            }, 'ct-circle');
 
-                                        pointData.element.replace(circle);
+                                            pointData.element.replace(circle);
+                                        }
                                     }
                                 }
-                            }
-                        ]}
-                    />
+                            ]}
+                        />
+                    </div>
                 </div>
             </div>
         );
